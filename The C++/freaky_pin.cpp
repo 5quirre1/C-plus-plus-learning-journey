f9a854bc156c1fc494a63ac2ec7257c8bac82490
Squirrel <rylancarey2011@gmail.com>
2024-11-07 21:35:08 -0800

    Create freaky_pin.cpp

---
+++ The C++/freaky_pin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+  
+  int pin = 0;
+  int tries = 0;
+  
+  std::cout << "BANK OF FREAKY\n";
+  
+  std::cout << "Enter your PIN: ";
+  std::cin >> pin;
+
+  tries++;
+
+  while (pin != 1234 && tries < 3) {
+    
+    std::cout << "That was wrong, Try again now or you're getting fucked. \n ";
+    std::cin >> pin;
+    tries++;
+    
+  }
+  
+  if (pin == 6969) {
+    
+    std::cout << "PIN accepted!\n";
+    std::cout << "You now have access.\n"; 
+    
+  }
+  
+}
