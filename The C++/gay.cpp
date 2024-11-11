f1f8dfd3db1160af1a33083269a0e67313a615f9
Squirrel <rylancarey2011@gmail.com>
2024-11-10 21:37:54 -0800

    Create gay.cpp

---
+++ The C++/gay.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string name;
+    std::cout << "Welcome to the gay tester\n";
+    std::cout << "First put your name\n";
+    std::cin >> name;
+    if (name == "wish") {
+        std::cout << "You are the most gayest person alive :fire:\n";
+    }
+    else if (name == "squirrel") {
+        std::cout << "You are mega gay\n";
+    }
+    else {
+        std::cout << "You are prob gay\n";
+    }
+return 0;
+}
+// thanks noma for help greg
