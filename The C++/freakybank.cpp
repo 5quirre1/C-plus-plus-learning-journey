db9e825351ef4fdd5725827c148d231c2fbd1a45
Squirrel <rylancarey2011@gmail.com>
2024-11-14 12:49:58 -0800

    Update freakybank.cpp

--- The C++/freakybank.cpp
+++ The C++/freakybank.cpp
@@ -10,13 +10,23 @@ std::cout << "Welcome to FreakyBank! You'll need to make an account to use this!
 std::cin >> option;
 if (option == "Signup" || option == "signup") {
     std::cout << "-------------------------------------------------------\n";
-    std::cout << "Username: " << account_name;
+    std::cout << "Username:\n";
+    std::cin >> account_name;
     std::cout << "-------------------------------------------------------\n";
-    std::cout << "Email\n";
+    std::cout << "Email:\n";
     std::cin >> email;
     std::cout << "-------------------------------------------------------\n";
+    std::cout << "Password:\n";
+    std::cin >> password;
+    std::cout << "-------------------------------------------------------\n";
+}
+std::cout << "Does this look alright? (y/n)\n";
+std::cin >> option;
+if (option == "y" || option == "Y" || option == "yes" || option == "Yes") {
+    std::cout << "Loading the dashboard..."; 
 }
 }
 int main() {
   greeting();
 }
+  
