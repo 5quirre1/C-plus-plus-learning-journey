de7e414420460719286d5617319c3c6bc4fd4299
Squirrel <rylancarey2011@gmail.com>
2024-11-15 08:18:44 -0800

    Update countdown.cpp

--- The C++/countdown.cpp
+++ The C++/countdown.cpp
@@ -1,23 +1,43 @@
-#include <iostream>
 #include <chrono>
 #include <thread>
-
+#include <iostream>
+#include <string>
 using namespace std;
+std::string account_name;
+std::string password;
+std::string email;
+void greeting() {
+    std::string option;
+    std::cout << "                                  Bank of Freaky\n";
+std::cout << "Welcome to FreakyBank! You'll need to make an account to use this! Type 'Signup' to start the signup process\n";
+std::cin >> option;
+if (option == "Signup" || option == "signup") {
+    std::cout << "-------------------------------------------------------\n";
+    std::cout << "Username:\n";
+    std::cin >> account_name;
+    std::cout << "-------------------------------------------------------\n";
+    std::cout << "Email:\n";
+    std::cin >> email;
+    std::cout << "-------------------------------------------------------\n";
+    std::cout << "Password:\n";
+    std::cin >> password;
+    std::cout << "-------------------------------------------------------\n";
+}
+std::cout << "Does this look alright? (y/n)\n";
+std::cin >> option;
+if (option == "y" || option == "Y" || option == "yes" || option == "Yes") {
+}
 
-int main()
-{
+}
+void home() {
+    this_thread::sleep_for(chrono::seconds(3));
+    std::cout << "Welcome " << account_name << "!";
     
-    cout << "Starting the countdown..." << endl;
+}
 
-    
-    for (int i = 5; i > 0; --i) {
-        
-        cout << i << " seconds remaining" << endl;
+int main() {
+  greeting();
+  std::cout << "Loading the dashboard..."; 
+  home();
 
-        
-        this_thread::sleep_for(chrono::seconds(1));
-    }
-
-    
-    cout << "Time's up!" << endl;
 }
