a932ac0905a3556415bc7bab03bc3b29165304b0
Squirrel <rylancarey2011@gmail.com>
2024-11-19 10:30:44 -0800

    Create letter.cpp

---
+++ The C++/letter.cpp
@@ -0,0 +1,10 @@
+// this is the letter I made when I first started to learn c++ lmao
+#include <iostream>
+
+int main() 
+{
+  std::cout << "11/7/24 4:57 PM\n";
+  std::cout << "Dear Self,\n";
+  std::cout << "I want to find myself a dev that can make games and real programs other than website and java.\n";
+  std::cout << "I'm going to learn C++ for this reason.";
+}
