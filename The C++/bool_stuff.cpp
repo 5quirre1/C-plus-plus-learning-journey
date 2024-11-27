53de54665be951f29e03a1966f0db0cd65a55de5
Squirrel <rylancarey2011@gmail.com>
2024-11-27 15:07:49 -0800

    Update bool_stuff.cpp

--- The C++/bool_stuff.cpp
+++ The C++/bool_stuff.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <string>
 
+void more() {
+int myAge;
+int old_asf = 18;
+std::cout << "Put your age and see if old asf\n";
+std::cin >> myAge;
+if (myAge  >=  old_asf) {
+    std::cout << "You're so fucking old\n";
+}
+else {
+    std::cout << "not old alert\n";
+}
+std::cout << "***********************************************\n";
+std::cout << "Thank you for using cause funsies and I'm learning yayaya";
 
+}
 int main() {
     std::string am_i_cooking;
+    std::string more_plz;
     std::cout << "True = 1, False = 0\n";
     std::cout << "22 = 22\n" << (22 == 22);
     std::cout << "\n";
@@ -16,12 +31,23 @@ int main() {
     std::cout << "Am I cooking with my learning\n";
     std::cin >> am_i_cooking;
     if (am_i_cooking == "yes" || am_i_cooking == "Yes" || am_i_cooking == "YES") {
-        std::cout << "Slayyyy";
+        std::cout << "Slayyyy\n";
     }
-    if (am_i_cooking == "no" || am_i_cooking == "No" || am_i_cooking == "NO") {
-        std::cout << "meanie....";
+    else if (am_i_cooking == "no" || am_i_cooking == "No" || am_i_cooking == "NO") {
+        std::cout << "meanie....\n";
     }
-else {
-    std::cout << "taking that as a yes frfr";
+    std::cout << "Wanna see more greg??\n";
+    std::cin >> more_plz;
+    if (more_plz == "yes" || more_plz == "Yes" || more_plz == "YES") {
+       more();
+    }
+    if (more_plz == "no" || more_plz == "No" || more_plz == "NO") {
+        std::cout << "No one likes you.";
+        exit(888888888);
+    }
+    else {
+        std::cout << "greg";
+        exit(888888888);
+    }
 }
-}
+
