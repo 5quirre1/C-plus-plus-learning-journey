f2ef83faedf3e3349004f81485724240597ded6f
Squirrel <rylancarey2011@gmail.com>
2024-11-27 14:19:04 -0800

    Create not_a_virus.cpp

---
+++ The C++/not_a_virus.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string fullname;
+  string your_gay;
+  cout << "put your full name, not scam real 2027\n";
+  getline(cin, fullname);
+  cout << "Your name is " << fullname << ", I just gave not_a_virus_just_mario_kart_8.virus into your laptop, it is not a virus, it is just free mario kart 8 trust real";
+}
