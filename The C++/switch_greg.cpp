972264b6aafa0118f8fcc3fe12a3c82cdacf9950
Squirrel <rylancarey2011@gmail.com>
2024-11-27 15:43:48 -0800

    Create switch_greg.cpp

---
+++ The C++/switch_greg.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main() {
+    int greg; // = put the case you want;
+    switch (greg) {
+        case 1:
+        std::cout << "greg1";
+        break;
+        case 2:
+        std::cout << "greg2";
+        break;
+        case 3:
+        std::cout << "greg3";
+        default:
+        std::cout << "greg is very skibidi";
+    }
+    
+}
