04148f6a61f2b3f6161bf0a0afe233f251010dee
Squirrel <rylancarey2011@gmail.com>
2024-11-27 16:55:32 -0800

    Update weird_letter_thing.cpp

--- The C++/weird_letter_thing.cpp
+++ The C++/weird_letter_thing.cpp
@@ -3,5 +3,11 @@
 
 int main() {
     std::string greg;
+    std::string gerg;
+    std::string* ptr = &gerg;
+    std::cout << "This is greg\n";
     std::cout << &greg;
+    std::cout << "\n";
+    std::cout << "This is gerg\n";
+    std::cout << ptr;
 }
