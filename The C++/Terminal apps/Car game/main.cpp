dbd1aa75f9efadbc0b668af5219d431b5051e34e
Squirrel <squirrelhomebrew@gmail.com>
2025-03-28 22:04:46 -0700

    Update main.cpp

--- The C++/Terminal apps/Car game/main.cpp
+++ The C++/Terminal apps/Car game/main.cpp
@@ -35,10 +35,10 @@ cout << "\n\nWelcome to car place. You will buy car now greg\n";
 
 cout << "Are you ready?\n";
 cin >> ready;
-if (ready == "yes" && "Yes"  && "YES") {
+if (ready == "yes" ||ready == "Yes"  ||ready == "YES") {
 actualcars();
   }
-  else if (ready == "no" && "No" && "NO" ) {
+  else if (ready == "no" ||ready == "No" ||ready == "NO" ) {
     cout << "Ok\n\n";
     cout << "*You walk out of the building and get on your 800 year old bike and ride back to your house.\n";
     cout << "When you get home, you get off your 800 year old bike.\nAs soon as you get off, the bike breaks into little pieces.\nYou sigh as you walk into your house. You sit on the couch and turn on the tv, a ad playing. \"Come on down to greg's famous old car shop place real no scam 2025 for cheap car that are no scam.\" You get up and go back to the car store.*\n";
